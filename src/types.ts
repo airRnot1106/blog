a7ebb4a2f36e46e98e553b59384429f2cad2244f
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-12 18:22:21 +0900

    feat(types): add FetchReturn type for API responses

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,7 @@
 export type ComponentProps = {
   class?: string | undefined;
 };
+
+export type FetchRetrun<T, E extends Error> =
+  | { data: T; error: undefined }
+  | { data: undefined; error: E };
