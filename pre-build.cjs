e9230ab54b55cada60b4f16b1eea71154c178b2f
airRnot1106 <yngw.rintaro@gmail.com>
2024-10-15 23:25:36 +0900

    refactor(pre-build): update fs and path imports to node style

--- pre-build.cjs
+++ pre-build.cjs
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
-const fs = require('fs');
-const path = require('path');
+const fs = require('node:fs');
+const path = require('node:path');
 
 async function downloadFont() {
   const fontUrl =
