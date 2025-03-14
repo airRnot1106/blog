5e0ad89b5c6e5f7ddd97518c6c2d38e230905c8e
airRnot1106 <yngw.rintaro@gmail.com>
2025-03-15 00:50:53 +0900

    feat: add eslint-plugin-tailwindcss for linting support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { FlatCompat } from '@eslint/eslintrc';
+import pluginTailwind from 'eslint-plugin-tailwindcss';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...pluginTailwind.configs['flat/recommended'],
 ];
 
 export default eslintConfig;
