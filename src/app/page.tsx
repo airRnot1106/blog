480067250bc68d9b553439efabef3772112de1e1
airRnot1106 <yngw.rintaro@gmail.com>
2025-03-16 23:06:24 +0900

    feat: add metadata for site title and icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,13 @@ import { SelfIntroductionSection } from '@/features/self-introduction/components
 import { SiteTitle } from '@/features/site-title/components/atoms/site-title/site-title';
 import { SiteLayout } from '@/features/site/components/molecules/site-layout';
 import { ZennArticleSection } from '@/features/zenn-article/components/molecules/zenn-article-section';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'airRnot.dev',
+  icons:
+    'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>✈️</text></svg>',
+};
 
 export default async function Home() {
   return (
