bf6525eda864ce64ebbab95a43283c51409b6faa
airRnot1106 <yngw.rintaro@gmail.com>
2025-03-23 21:11:09 +0900

    feat(heading): update Heading component to use hgroup wrapper

--- src/components/base/atoms/heading/heading.tsx
+++ src/components/base/atoms/heading/heading.tsx
@@ -1,13 +1,29 @@
-import { type ComponentProps, type FC, memo } from 'react';
+import { type ComponentProps, type FC, type ReactNode, memo } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 export type HeadingProps = Readonly<
-  ComponentProps<'div'> & {
+  ComponentProps<'hgroup'> & {
     title: string;
     subtitle: string;
     as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
   }
 >;
+
+const HeaddingWrapper: FC<
+  ComponentProps<'hgroup'> & { children: ReactNode }
+> = ({ children, className, ...rest }) => {
+  return (
+    <hgroup
+      className={twMerge(
+        'grid grid-rows-[auto_2px] grid-cols-[auto_auto_1fr] items-center',
+        className,
+      )}
+      {...rest}
+    >
+      {children}
+    </hgroup>
+  );
+};
 
 export const Heading: FC<HeadingProps> = memo(
   ({ title, subtitle, as, className, ...rest }) => {
@@ -18,86 +34,80 @@ export const Heading: FC<HeadingProps> = memo(
     );
 
     return (
-      <div
-        className={twMerge(
-          'grid grid-rows-[auto_2px] grid-cols-[auto_auto_1fr] items-center',
-          className,
-        )}
-        {...rest}
-      >
+      <>
         {as === 'h1' && (
-          <>
+          <HeaddingWrapper className={className} {...rest}>
             <h1 className="text-[clamp(1.5rem,1.227rem_+_1.36vw,2.25rem)] font-bold">
               {title}
             </h1>
             {Divider}
-            <span className="text-[clamp(1.125rem,0.989rem_+_0.68vw,1.5rem)] font-ich1q mt-2">
+            <p className="text-[clamp(1.125rem,0.989rem_+_0.68vw,1.5rem)] font-ich1q mt-2">
               {subtitle}
-            </span>
+            </p>
             {Border}
-          </>
+          </HeaddingWrapper>
         )}
         {as === 'h2' && (
-          <>
+          <HeaddingWrapper className={className} {...rest}>
             <h2 className="text-[clamp(1.25rem,1.023rem_+_1.14vw,1.875rem)] font-bold">
               {title}
             </h2>
             {Divider}
-            <span className="text-[clamp(1rem,0.909rem_+_0.45vw,1.25rem)] font-ich1q mt-2">
+            <p className="text-[clamp(1rem,0.909rem_+_0.45vw,1.25rem)] font-ich1q mt-2">
               {subtitle}
-            </span>
+            </p>
             {Border}
-          </>
+          </HeaddingWrapper>
         )}
         {as === 'h3' && (
-          <>
+          <HeaddingWrapper className={className} {...rest}>
             <h3 className="text-[clamp(1.125rem,0.989rem_+_0.68vw,1.5rem)] font-bold">
               {title}
             </h3>
             {Divider}
-            <span className="text-[clamp(0.875rem,0.784rem_+_0.45vw,1.125rem)] font-ich1q mt-2">
+            <p className="text-[clamp(0.875rem,0.784rem_+_0.45vw,1.125rem)] font-ich1q mt-2">
               {subtitle}
-            </span>
+            </p>
             {Border}
-          </>
+          </HeaddingWrapper>
         )}
         {as === 'h4' && (
-          <>
+          <HeaddingWrapper className={className} {...rest}>
             <h4 className="text-[clamp(1rem,0.909rem_+_0.45vw,1.25rem)] font-bold">
               {title}
             </h4>
             {Divider}
-            <span className="text-[clamp(0.75rem,0.659rem_+_0.45vw,1rem)] font-ich1q mt-2">
+            <p className="text-[clamp(0.75rem,0.659rem_+_0.45vw,1rem)] font-ich1q mt-2">
               {subtitle}
-            </span>
+            </p>
             {Border}
-          </>
+          </HeaddingWrapper>
         )}
         {as === 'h5' && (
-          <>
+          <HeaddingWrapper className={className} {...rest}>
             <h5 className="text-[clamp(1rem,0.909rem_+_0.45vw,1.25rem)] font-bold">
               {title}
             </h5>
             {Divider}
-            <span className="text-[clamp(0.75rem,0.659rem_+_0.45vw,1rem)] font-ich1q mt-2">
+            <p className="text-[clamp(0.75rem,0.659rem_+_0.45vw,1rem)] font-ich1q mt-2">
               {subtitle}
-            </span>
+            </p>
             {Border}
-          </>
+          </HeaddingWrapper>
         )}
         {as === 'h6' && (
-          <>
+          <HeaddingWrapper className={className} {...rest}>
             <h6 className="text-[clamp(1rem,0.909rem_+_0.45vw,1.25rem)] font-bold">
               {title}
             </h6>
             {Divider}
-            <span className="text-[clamp(0.75rem,0.659rem_+_0.45vw,1rem)] font-ich1q mt-2">
+            <p className="text-[clamp(0.75rem,0.659rem_+_0.45vw,1rem)] font-ich1q mt-2">
               {subtitle}
-            </span>
+            </p>
             {Border}
-          </>
+          </HeaddingWrapper>
         )}
-      </div>
+      </>
     );
   },
 );
