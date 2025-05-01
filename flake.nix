e1e3254eabba08ddb0218df585a391fcd5615b97
airRnot1106 <yngw.rintaro@gmail.com>
2025-05-01 23:35:45 +0900

    chore: add flake for dev environment setup

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "airRnot's blog";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs
+            pnpm
+          ];
+        };
+      }
+    );
+}
