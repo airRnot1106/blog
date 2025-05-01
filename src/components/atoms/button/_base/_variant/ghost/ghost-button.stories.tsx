0720641e8ee3becd1881dbe4745f6ce8b1a512f0
airRnot1106 <airRnot1106@users.noreply.github.com>
2025-05-02 01:40:56 +0900

    feat: add button

---
+++ src/components/atoms/button/_base/_variant/ghost/ghost-button.stories.tsx
@@ -0,0 +1,102 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { expect, fn, userEvent, within } from '@storybook/test';
+import { ArrowRight } from 'lucide-react';
+import { css } from '../../../../../../../styled-system/css';
+import { GhostButton } from './ghost-button';
+
+const meta: Meta<typeof GhostButton> = {
+  title: 'Components/Atoms/Button/_Base/_Variant/Ghost/GhostButton',
+  component: GhostButton,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    (Story) => (
+      <div
+        className={css({
+          padding: '1rem',
+        })}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof GhostButton>;
+
+const handleClick = fn();
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <GhostButton onClick={handleClick} size="md">
+        Button
+      </GhostButton>
+    );
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = canvas.getByRole('button');
+    await userEvent.click(button);
+    expect(handleClick).toHaveBeenCalled();
+  },
+};
+
+export const Size: Story = {
+  render: () => {
+    return (
+      <ul
+        className={css({
+          display: 'flex',
+          alignItems: 'center',
+          gap: '1rem',
+        })}
+      >
+        {(['fit-content', 'xs', 'sm', 'md', 'lg', 'xl'] as const).map(
+          (size) => (
+            <li key={size}>
+              <GhostButton size={size}>Button</GhostButton>
+            </li>
+          ),
+        )}
+      </ul>
+    );
+  },
+};
+
+export const Icon: Story = {
+  render: () => {
+    return (
+      <GhostButton
+        className={css({
+          columnGap: '0.25rem',
+        })}
+        icon={<ArrowRight />}
+        iconPosition="right"
+        size="md"
+      >
+        もっとみる
+      </GhostButton>
+    );
+  },
+};
+
+export const Disabled: Story = {
+  render: () => {
+    return (
+      <GhostButton disabled onClick={handleClick} size="md">
+        Button
+      </GhostButton>
+    );
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const button = canvas.getByRole('button');
+    await userEvent.click(button);
+    expect(handleClick).not.toHaveBeenCalled();
+  },
+};
