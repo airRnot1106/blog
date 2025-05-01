e3f40c36b8946247869c1adc27b77f6ca239d09d
airRnot1106 <yngw.rintaro@gmail.com>
2025-05-02 00:00:08 +0900

    feat: Add Vitest testing and coverage configuration

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { coverageConfigDefaults, defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/build/**',
+      '**/out/**',
+      '.direnv',
+    ],
+    coverage: {
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        'src/**/index.{js,jsx,ts,tsx}',
+      ],
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+});
