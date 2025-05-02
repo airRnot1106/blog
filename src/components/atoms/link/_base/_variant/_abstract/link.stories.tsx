54d1530bf8365277ee0c26a322f26f1eda7292ee
airRnot1106 <yngw.rintaro@gmail.com>
2025-05-03 01:00:33 +0900

    feat: add link

---
+++ src/components/atoms/link/_base/_variant/_abstract/link.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { css } from '../../../../../../../styled-system/css';
+import { Link } from './link';
+
+const meta: Meta<typeof Link> = {
+  title: 'Components/Atoms/Link/_Base/_Variant/_Abstract/Link',
+  component: Link,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    (Story) => (
+      <div
+        className={css({
+          padding: '1rem',
+        })}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Link>;
+
+export const Default: Story = {
+  render: () => {
+    return <Link href="#">Link</Link>;
+  },
+};
