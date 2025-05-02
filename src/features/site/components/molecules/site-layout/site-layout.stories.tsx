69390c51a5811a27e708bbb72f68229ebc7e1d41
airRnot1106 <yngw.rintaro@gmail.com>
2025-05-02 14:28:31 +0900

    feat: add site-layout

---
+++ src/features/site/components/molecules/site-layout/site-layout.stories.tsx
@@ -0,0 +1,101 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import type { FC } from 'react';
+import { SiteLayout } from './site-layout';
+
+const meta: Meta<typeof SiteLayout> = {
+  title: 'Features/Site/Components/Molecules/SiteLayout',
+  component: SiteLayout,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SiteLayout>;
+
+const Main: FC = () => {
+  return (
+    <main>
+      <h1>Site Layout</h1>
+      <p>This is the main content area.</p>
+    </main>
+  );
+};
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <SiteLayout>
+        <Main />
+      </SiteLayout>
+    );
+  },
+};
+
+const Component = (
+  <SiteLayout>
+    <Main />
+  </SiteLayout>
+);
+
+export const XSmall: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xs',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Small: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'sm',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Medium: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'md',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Large: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'lg',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const XLarge: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xl',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
