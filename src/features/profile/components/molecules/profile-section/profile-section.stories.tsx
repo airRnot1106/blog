ed69fdf6e25b1df0d896a2d1f5c02fdabed45493
airRnot1106 <yngw.rintaro@gmail.com>
2025-05-05 01:37:19 +0900

    feat: add profile-section

---
+++ src/features/profile/components/molecules/profile-section/profile-section.stories.tsx
@@ -0,0 +1,95 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { css } from '../../../../../../styled-system/css';
+import { ProfileSection } from './profile-section';
+
+const meta: Meta<typeof ProfileSection> = {
+  title: 'Features/Profile/Components/Molecules/ProfileSection',
+  component: ProfileSection,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    (Story) => (
+      <div
+        className={css({
+          padding: '1rem',
+        })}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ProfileSection>;
+
+export const Default: Story = {
+  render: () => {
+    return <ProfileSection />;
+  },
+};
+
+const Component = <ProfileSection />;
+
+export const XSmall: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xs',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Small: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'sm',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Medium: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'md',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Large: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'lg',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const XLarge: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xl',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
---
+++ src/features/profile/consts.ts
@@ -0,0 +1,22 @@
+export const PROFILE_LINKS = [
+  {
+    label: 'GitHub',
+    url: 'https://github.com/airRnot1106',
+  },
+  {
+    label: 'Bluesky',
+    url: 'https://bsky.app/profile/airrnot.bsky.social',
+  },
+  {
+    label: 'mixi2',
+    url: 'https://mixi.social/@airRnot',
+  },
+  {
+    label: 'Zenn',
+    url: 'https://zenn.dev/airrnot1106',
+  },
+  {
+    label: 'Qiita',
+    url: 'https://qiita.com/airRnot1106',
+  },
+] as const satisfies { label: string; url: string }[];
