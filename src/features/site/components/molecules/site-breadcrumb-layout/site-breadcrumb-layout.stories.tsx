3242daf59de9e5d789914671e6913386f47f9852
airRnot1106 <yngw.rintaro@gmail.com>
2025-05-06 23:03:00 +0900

    feat: add site-breadcrumb-layout

---
+++ src/features/site/components/molecules/site-breadcrumb-layout/site-breadcrumb-layout.stories.tsx
@@ -0,0 +1,125 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { css } from '../../../../../../styled-system/css';
+import { SiteBreadcrumbLayout } from './site-breadcrumb-layout';
+
+const meta: Meta<typeof SiteBreadcrumbLayout> = {
+  title: 'Features/Site/Components/Molecules/SiteBreadcrumbLayout',
+  component: SiteBreadcrumbLayout,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    (Story) => (
+      <div
+        className={css({
+          padding: '1rem',
+        })}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof SiteBreadcrumbLayout>;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <SiteBreadcrumbLayout
+        items={[
+          {
+            label: 'TOP',
+            href: '/',
+          },
+          {
+            label: '記事一覧',
+            href: '/articles',
+          },
+        ]}
+      >
+        <div>Contents</div>
+      </SiteBreadcrumbLayout>
+    );
+  },
+};
+
+const Component = (
+  <SiteBreadcrumbLayout
+    items={[
+      {
+        label: 'TOP',
+        href: '/',
+      },
+      {
+        label: '記事一覧',
+        href: '/articles',
+      },
+    ]}
+  >
+    <div>Contents</div>
+  </SiteBreadcrumbLayout>
+);
+
+export const XSmall: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xs',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Small: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'sm',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Medium: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'md',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Large: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'lg',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const XLarge: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xl',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
