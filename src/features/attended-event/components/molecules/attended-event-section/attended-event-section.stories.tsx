7dc60f3b8f429af87c38111653d7703e0ef66e65
airRnot1106 <yngw.rintaro@gmail.com>
2025-05-06 20:51:46 +0900

    feat: attended-event-section

---
+++ src/features/attended-event/components/molecules/attended-event-section/attended-event-section.stories.tsx
@@ -0,0 +1,95 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { css } from '../../../../../../styled-system/css';
+import { AttendedEventSection } from './attended-event-section';
+
+const meta: Meta<typeof AttendedEventSection> = {
+  title: 'Features/AttendedEvent/Components/Molecules/AttendedEventSection',
+  component: AttendedEventSection,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    (Story) => (
+      <div
+        className={css({
+          padding: '1rem',
+        })}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof AttendedEventSection>;
+
+export const Default: Story = {
+  render: () => {
+    return <AttendedEventSection />;
+  },
+};
+
+const Component = <AttendedEventSection />;
+
+export const XSmall: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xs',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Small: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'sm',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Medium: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'md',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Large: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'lg',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const XLarge: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xl',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
