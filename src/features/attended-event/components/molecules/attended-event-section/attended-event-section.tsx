9be887877bb62e790343aba5afdf9e1f857bdb32
airRnot1106 <yngw.rintaro@gmail.com>
2025-05-06 20:51:46 +0900

    feat: attended-event-section

---
+++ src/features/attended-event/components/molecules/attended-event-section/attended-event-section.tsx
@@ -0,0 +1,25 @@
+import type { ComponentProps, FC } from 'react';
+import { css } from '../../../../../../styled-system/css';
+import { Heading } from '../../../../../components/atoms/heading';
+import { AttendedEventList } from '../attended-event-list';
+
+export type AttendedEventSection = ComponentProps<'section'>;
+
+export const AttendedEventSection: FC<AttendedEventSection> = ({
+  className,
+  ...rest
+}) => {
+  return (
+    <section className={className} {...rest}>
+      <Heading as="h2" subtitle="さんかいべんと" title="参加イベント" />
+      <div
+        className={css({
+          display: 'grid',
+          paddingBlock: '1rem',
+        })}
+      >
+        <AttendedEventList />
+      </div>
+    </section>
+  );
+};
