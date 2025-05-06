e0ffc2ab61c81cc3ffacdf993cb9f7bae9e77f42
airRnot1106 <yngw.rintaro@gmail.com>
2025-05-06 22:13:16 +0900

    feat: add article-card-list

---
+++ src/features/article/components/molecules/article-card-list/article-card-list.stories.tsx
@@ -0,0 +1,96 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { css } from '../../../../../../styled-system/css';
+import { articles } from '../../../mocks';
+import { ArticleCardListPresentational } from './article-card-list.presentational';
+
+const meta: Meta<typeof ArticleCardListPresentational> = {
+  title: 'Features/Article/Components/Molecules/ArticleCardList',
+  component: ArticleCardListPresentational,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    (Story) => (
+      <div
+        className={css({
+          padding: '1rem',
+        })}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof ArticleCardListPresentational>;
+
+export const Default: Story = {
+  render: () => {
+    return <ArticleCardListPresentational articles={articles} />;
+  },
+};
+
+const Component = <ArticleCardListPresentational articles={articles} />;
+
+export const XSmall: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xs',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Small: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'sm',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Medium: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'md',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Large: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'lg',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const XLarge: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xl',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
---
+++ src/features/article/components/molecules/article-card-list/index.ts
@@ -0,0 +1,2 @@
+export type { ArticleCardListFetcherProps as ArticleCardListProps } from './article-card-list.fetcher';
+export { ArticleCardListFetcher as ArticleCardList } from './article-card-list.fetcher';
