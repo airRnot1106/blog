27107a71d676263218c9414c334756069c920de8
airRnot1106 <yngw.rintaro@gmail.com>
2025-05-06 16:42:17 +0900

    feat: add fetch-result-message

---
+++ src/features/fetch-result/components/molecules/fetch-result-message/fetch-result-message.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { css } from '../../../../../../styled-system/css';
+import { FetchResultMessage } from './fetch-result-message';
+
+const meta: Meta<typeof FetchResultMessage> = {
+  title: 'Features/FetchResult/Components/Molecules/FetchResultMessage',
+  component: FetchResultMessage,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    (Story) => (
+      <div
+        className={css({
+          padding: '1rem',
+        })}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof FetchResultMessage>;
+
+export const ErrorMessage: Story = {
+  render: () => {
+    return <FetchResultMessage variant="error" />;
+  },
+};
