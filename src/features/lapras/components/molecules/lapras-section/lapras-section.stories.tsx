d8e47305f3234917398e942b2e9e999e2e4ed1bb
airRnot1106 <airRnot1106@users.noreply.github.com>
2025-05-06 16:42:47 +0900

    feat: add lapras-section

---
+++ src/features/lapras/components/molecules/lapras-section/lapras-section.stories.tsx
@@ -0,0 +1,115 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { css } from '../../../../../../styled-system/css';
+import { FetchResultMessage } from '../../../../fetch-result/components/molecules/fetch-result-message';
+import { LaprasImagePresentational } from '../../atoms/lapras-image/lapras-image.presentational';
+import { LaprasSection } from './lapras-section';
+
+const meta: Meta<typeof LaprasSection> = {
+  title: 'Features/Lapras/Components/Molecules/LaprasSection',
+  component: LaprasSection,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    (Story) => (
+      <div
+        className={css({
+          padding: '1rem',
+        })}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof LaprasSection>;
+
+export const Default: Story = {
+  render: () => {
+    return (
+      <LaprasSection>
+        <LaprasImagePresentational src="https://media.lapras.com/media/public_setting/RMNHLTF/eecd161ed5ab498381a9c14b2f729ee7.png" />
+      </LaprasSection>
+    );
+  },
+};
+
+export const Failed: Story = {
+  render: () => {
+    return (
+      <LaprasSection>
+        <FetchResultMessage variant="error" />
+      </LaprasSection>
+    );
+  },
+};
+
+const Component = (
+  <LaprasSection>
+    <LaprasImagePresentational src="https://media.lapras.com/media/public_setting/RMNHLTF/eecd161ed5ab498381a9c14b2f729ee7.png" />
+  </LaprasSection>
+);
+
+export const XSmall: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xs',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Small: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'sm',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Medium: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'md',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const Large: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'lg',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
+
+export const XLarge: Story = {
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xl',
+    },
+  },
+  render: () => {
+    return Component;
+  },
+};
---
+++ src/features/lapras/components/molecules/lapras-section/lapras-section.tsx
@@ -0,0 +1,49 @@
+import { SquareArrowOutUpRight } from 'lucide-react';
+import type { ComponentProps, FC, ReactNode } from 'react';
+import { css } from '../../../../../../styled-system/css';
+import { Heading } from '../../../../../components/atoms/heading';
+import { LinkButton } from '../../../../../components/atoms/link-button';
+
+export type LaprasSectionProps = ComponentProps<'section'> & {
+  children: ReactNode;
+};
+
+export const LaprasSection: FC<LaprasSectionProps> = ({
+  children,
+  className,
+  ...rest
+}) => {
+  return (
+    <section className={className} {...rest}>
+      <Heading as="h2" subtitle="lapras" title="Lapras" />
+      <div
+        className={css({
+          display: 'grid',
+          justifyContent: 'center',
+          paddingBlock: '2rem',
+        })}
+      >
+        {children}
+      </div>
+      <div
+        className={css({
+          display: 'grid',
+          justifyContent: 'center',
+        })}
+      >
+        <LinkButton
+          className={css({
+            columnGap: '0.5rem',
+          })}
+          href="https://lapras.com/public/airrnot"
+          icon={<SquareArrowOutUpRight size="20" />}
+          iconPosition="right"
+          target="_blank"
+          variant="normal"
+        >
+          公開ページへ
+        </LinkButton>
+      </div>
+    </section>
+  );
+};
