291bb0a6b005b1c1b8e8c7fe784ab78d0a9ab46f
airRnot1106 <yngw.rintaro@gmail.com>
2025-05-10 23:54:22 +0900

    feat: add article page

--- src/app/articles/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { SiteContentLayout } from '../../features/site-content/components/molecules/site-content-layout';
-
-export default function ArticlesLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <SiteContentLayout>{children}</SiteContentLayout>;
-}
