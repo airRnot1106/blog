be2b98c181e71e1c5641dbcca95ffc6d2a223592
airRnot1106 <yngw.rintaro@gmail.com>
2025-05-10 23:48:38 +0900

    feat: add blog-article-toc-navigation-section

---
+++ src/features/blog-article-toc/components/molecules/blog-article-toc-navigation-section/index.ts
@@ -0,0 +1,2 @@
+export type { BlogArticleTocNavigationSectionContainerProps as BlogArticleTocNavigationSectionProps } from './blog-article-toc-navigation-section.container';
+export { BlogArticleTocNavigationSectionContainer as BlogArticleTocNavigationSection } from './blog-article-toc-navigation-section.container';
