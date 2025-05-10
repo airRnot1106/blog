f4c6cc1968d06e7e28f6d436a28aa8e75812113d
airRnot1106 <yngw.rintaro@gmail.com>
2025-05-10 13:39:51 +0900

    feat: add anchor-heading and refactor

---
+++ src/components/atoms/heading/_base/_variant/anchor/anchor-heading.stories.tsx
@@ -0,0 +1,105 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { css } from '../../../../../../../styled-system/css';
+import { AnchorHeading } from './anchor-heading';
+
+const meta: Meta<typeof AnchorHeading> = {
+  title: 'Components/Atoms/Heading/_Base/_Variant/Anchor/AnchorHeading',
+  component: AnchorHeading,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    (Story) => (
+      <div
+        className={css({
+          padding: '1rem',
+        })}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof AnchorHeading>;
+
+export const H1: Story = {
+  render: () => {
+    return (
+      <AnchorHeading
+        as="h1"
+        href="#タイトル1"
+        subtitle="さぶたいとる"
+        title="タイトル1"
+      />
+    );
+  },
+};
+
+export const H2: Story = {
+  render: () => {
+    return (
+      <AnchorHeading
+        as="h2"
+        href="#タイトル2"
+        subtitle="さぶたいとる"
+        title="タイトル2"
+      />
+    );
+  },
+};
+
+export const H3: Story = {
+  render: () => {
+    return (
+      <AnchorHeading
+        as="h3"
+        href="#タイトル3"
+        subtitle="さぶたいとる"
+        title="タイトル3"
+      />
+    );
+  },
+};
+
+export const H4: Story = {
+  render: () => {
+    return (
+      <AnchorHeading
+        as="h4"
+        href="#タイトル4"
+        subtitle="さぶたいとる"
+        title="タイトル4"
+      />
+    );
+  },
+};
+
+export const H5: Story = {
+  render: () => {
+    return (
+      <AnchorHeading
+        as="h5"
+        href="#タイトル5"
+        subtitle="さぶたいとる"
+        title="タイトル5"
+      />
+    );
+  },
+};
+
+export const H6: Story = {
+  render: () => {
+    return (
+      <AnchorHeading
+        as="h6"
+        href="#タイトル6"
+        subtitle="さぶたいとる"
+        title="タイトル6"
+      />
+    );
+  },
+};
