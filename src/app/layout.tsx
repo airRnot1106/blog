76d11821c7282050741f459b6e946b0ac7ac274b
airRnot1106 <airRnot1106@users.noreply.github.com>
2025-05-12 00:39:34 +0900

    feat: add ogp to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,19 +8,49 @@ import { ich1q, zenkaku } from './fonts';
 
 import './globals.css';
 
+const title = 'airRnot.dev';
+const description =
+  'airRnotのブログです。主にフロントエンドに関する記事をまとめています。';
+const url = 'https://airrnot.dev';
+
 export const metadata: Metadata = {
   title: {
-    template: '%s | airRnot.dev',
-    default: 'airRnot.dev',
+    template: `%s | ${title}`,
+    default: title,
   },
-  description:
-    'airRnotのブログです。主にフロントエンドに関する記事をまとめています。',
+  description: description,
   icons:
     'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>✈️</text></svg>',
   alternates: {
     types: {
-      'application/rss+xml': 'https://airrnot.dev/articles/feed.xml',
+      'application/rss+xml': `${url}/articles/feed.xml`,
     },
+  },
+
+  openGraph: {
+    title: title,
+    description: description,
+    url: url,
+    type: 'article',
+    images: [
+      {
+        url: `${url}/api/og?title=${encodeURIComponent(title)}`,
+        width: 1200,
+        height: 630,
+        alt: title,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: title,
+    description: description,
+    images: [
+      {
+        url: `${url}/api/og?title=${encodeURIComponent(title)}`,
+        alt: title,
+      },
+    ],
   },
 };
 
