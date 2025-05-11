a156f8f4619c8a99ee83fa4d53edb68432d9c56d
airRnot1106 <airRnot1106@users.noreply.github.com>
2025-05-11 17:35:47 +0900

    feat: add site-navigation-link

---
+++ src/features/site/components/atoms/site-navigation-link/site-navigation-link.tsx
@@ -0,0 +1,42 @@
+import Link, { type LinkProps } from 'next/link';
+import type { ComponentProps, FC } from 'react';
+import { css, cx } from '../../../../../../styled-system/css';
+import type { SiteNavigationLink as SiteNavigationLinkType } from '../../../types';
+
+export type SiteNavigationLinkProps = Omit<
+  LinkProps & ComponentProps<'a'>,
+  'href'
+> & {
+  link: SiteNavigationLinkType;
+};
+
+export const SiteNavigationLink: FC<SiteNavigationLinkProps> = ({
+  link,
+  className,
+  ...rest
+}) => {
+  return (
+    <Link
+      className={cx(
+        className,
+        css({
+          display: 'grid',
+          gridTemplateColumns: 'auto 1fr',
+          alignItems: 'center',
+          columnGap: '0.875rem',
+          paddingBlock: '0.5rem',
+          paddingInline: '1.25rem',
+          _hover: {
+            backgroundColor:
+              'color-mix(in oklab, var(--colors-base), var(--color-mix-base) 7%)',
+          },
+        }),
+      )}
+      href={link.href}
+      {...rest}
+    >
+      {link.icon}
+      <span>{link.label}</span>
+    </Link>
+  );
+};
