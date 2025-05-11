0244ebd924779e31f56735e6e3be76cf79ef3aca
airRnot1106 <airRnot1106@users.noreply.github.com>
2025-05-11 13:07:02 +0000

    deploy: eb5b85c414b0a327944e1b41cfb963886fbefb18

---
+++ assets/site-breadcrumb-layout.stories-mPnzg3Ma.js
@@ -0,0 +1,63 @@
+import{j as r}from"./jsx-runtime-DoEZbXM1.js";import{c as d}from"./css-iA79nXp1.js";import{c as X}from"./cx-BFURdk0p.js";import{S as _}from"./site-breadcrumb-Cv3w_fq_.js";import"./jsx-runtime-Bw5QeaCk.js";import"./link-button-DcK4i4oQ.js";import"./ghost-link-button-B4OS-esE.js";import"./link-button-C7RiLLtY.js";import"./link-CiQcZn3-.js";import"./_commonjsHelpers-CqkleIqs.js";import"./router-context.shared-runtime-C3rDQPqq.js";import"./index-Za3YRiuF.js";import"./add-base-path-Yz4dcL5G.js";import"./use-merged-ref-vu_KmdPm.js";import"./button-abs7jE4F.js";import"./ghost-button-DVSEpKed.js";import"./button-CFgMdbM0.js";import"./cva-CuDB4TaM.js";import"./normal-button-Be3dRcwo.js";import"./normal-link-button-CMCePiL0.js";const i=({items:p,children:T,className:O,...P})=>r.jsxs("div",{className:X(O,d({display:"grid",gridAutoFlow:"row",rowGap:"1rem"})),...P,children:[r.jsx(_,{className:d({paddingBlock:"1rem",borderBottom:"1px solid",borderBottomColor:"muted/20"}),items:p}),T]});i.__docgenInfo={description:"",methods:[],displayName:"SiteBreadcrumbLayout",props:{items:{required:!0,tsType:{name:"intersection['items']",raw:"SiteBreadcrumbProps['items']"},description:""},children:{required:!0,tsType:{name:"ReactNode"},description:""}}};const rr={component:i,tags:["autodocs"],parameters:{layout:"fullscreen"},decorators:[p=>r.jsx("div",{className:d({padding:"1rem"}),children:r.jsx(p,{})})]},t={render:()=>r.jsx(i,{items:[{label:"TOP",href:"/"},{label:"記事一覧",href:"/articles"}],children:r.jsx("div",{children:"Contents"})})},e=r.jsx(i,{items:[{label:"TOP",href:"/"},{label:"記事一覧",href:"/articles"}],children:r.jsx("div",{children:"Contents"})}),o={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"xs"}},render:()=>e},a={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"sm"}},render:()=>e},s={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"md"}},render:()=>e},n={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"lg"}},render:()=>e},m={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"xl"}},render:()=>e};var c,u,l;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  render: () => {
+    return <SiteBreadcrumbLayout items={[{
+      label: 'TOP',
+      href: '/'
+    }, {
+      label: '記事一覧',
+      href: '/articles'
+    }]}>
+        <div>Contents</div>
+      </SiteBreadcrumbLayout>;
+  }
+}`,...(l=(u=t.parameters)==null?void 0:u.docs)==null?void 0:l.source}}};var g,f,w;o.parameters={...o.parameters,docs:{...(g=o.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xs'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(w=(f=o.parameters)==null?void 0:f.docs)==null?void 0:w.source}}};var x,S,v;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'sm'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(v=(S=a.parameters)==null?void 0:S.docs)==null?void 0:v.source}}};var b,h,j;s.parameters={...s.parameters,docs:{...(b=s.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'md'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(j=(h=s.parameters)==null?void 0:h.docs)==null?void 0:j.source}}};var C,V,y;n.parameters={...n.parameters,docs:{...(C=n.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'lg'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(y=(V=n.parameters)==null?void 0:V.docs)==null?void 0:y.source}}};var B,L,N;m.parameters={...m.parameters,docs:{...(B=m.parameters)==null?void 0:B.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xl'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(N=(L=m.parameters)==null?void 0:L.docs)==null?void 0:N.source}}};const er=["Default","XSmall","Small","Medium","Large","XLarge"];export{t as Default,n as Large,s as Medium,a as Small,m as XLarge,o as XSmall,er as __namedExportsOrder,rr as default};
