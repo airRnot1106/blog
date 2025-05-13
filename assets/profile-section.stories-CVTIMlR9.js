b58be78784a25bb44286c6a5708d215185a946b5
airRnot1106 <airRnot1106@users.noreply.github.com>
2025-05-13 14:07:09 +0000

    deploy: 7d489f66f4d5ff7311befe2b6d81578f62b26e13

---
+++ assets/profile-section.stories-CVTIMlR9.js
@@ -0,0 +1,55 @@
+import{j as r}from"./jsx-runtime-DoEZbXM1.js";import{c as e}from"./css-iA79nXp1.js";import{c as _}from"./cx-BFURdk0p.js";import{H as z}from"./heading-CEno6Knd.js";import{I as A}from"./image-icon-CLulPJ8U.js";import{L as P}from"./link-D8HMmSCG.js";import"./jsx-runtime-Bw5QeaCk.js";import"./heading-CsMin62-.js";import"./cva-CuDB4TaM.js";import"./anchor-heading-BdPI4WXK.js";import"./link-CiQcZn3-.js";import"./_commonjsHelpers-CqkleIqs.js";import"./router-context.shared-runtime-C3rDQPqq.js";import"./index-Za3YRiuF.js";import"./add-base-path-Yz4dcL5G.js";import"./use-merged-ref-vu_KmdPm.js";import"./circle-image-icon-DGM44Y2V.js";import"./image-icon-C27l0v7T.js";import"./image-BcZ0HIHc.js";import"./index-DqQFE8CJ.js";import"./image-context-CYaGmMjm.js";import"./index-DDr4uD8U.js";import"./square-image-icon-w6xnlA5R.js";import"./underline-link-BKNhLiPR.js";import"./link-DOkctWu4.js";const X=[{label:"GitHub",url:"https://github.com/airRnot1106"},{label:"Bluesky",url:"https://bsky.app/profile/airrnot.bsky.social"},{label:"mixi2",url:"https://mixi.social/@airRnot"},{label:"Zenn",url:"https://zenn.dev/airrnot1106"},{label:"Qiita",url:"https://qiita.com/airRnot1106"}],p=({className:c,...B})=>r.jsxs("section",{className:_(c),...B,children:[r.jsx(z,{as:"h2",subtitle:"about",title:"About"}),r.jsxs("div",{className:e({display:"grid",gridAutoFlow:"row",justifyItems:"center",rowGap:"1rem",paddingBlock:"1rem",borderBottom:"1px solid",borderBottomColor:"muted/20"}),children:[r.jsx(A,{alt:"",className:e({width:"clamp(8rem, 4.364rem + 15.52vw, 16rem)"}),height:"300",priority:!0,size:"fit-content",src:"/images/profile/airrnot.webp",variant:"circle",width:"300"}),r.jsx("span",{className:e({fontSize:"clamp(1.5rem, 1.159rem + 1.45vw, 2.25rem)",fontWeight:"bold"}),children:"airRnot"}),r.jsxs("p",{className:e({fontSize:"clamp(0.75rem, 0.58rem + 0.73vw, 1.125rem)",wordBreak:"keep-all",textAlign:"center"}),children:["フロントエンドをメインにやっています",r.jsx("br",{}),"趣味はdotfiles盆栽とディレクトリ構成について考えることです"]})]}),r.jsx("ul",{className:e({display:"grid",gridAutoFlow:"column",justifyContent:"center",paddingBlock:"1rem"}),children:X.map(({label:l,url:R})=>r.jsx("li",{className:e({width:"fit-content","&:not(:last-child)":{_after:{content:'"/"',marginInline:"0.5rem"}}}),children:r.jsx(P,{className:e({fontSize:"clamp(1rem, 0.886rem + 0.48vw, 1.25rem)"}),href:R,variant:"underline",children:l})},l))})]});p.__docgenInfo={description:"",methods:[],displayName:"ProfileSection"};const mr={component:p,tags:["autodocs"],parameters:{layout:"fullscreen"},decorators:[c=>r.jsx("div",{className:e({padding:"1rem"}),children:r.jsx(c,{})})]},o={render:()=>r.jsx(p,{})},t=r.jsx(p,{}),a={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"xs"}},render:()=>t},s={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"sm"}},render:()=>t},n={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"md"}},render:()=>t},i={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"lg"}},render:()=>t},m={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"xl"}},render:()=>t};var d,u,g;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: () => {
+    return <ProfileSection />;
+  }
+}`,...(g=(u=o.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var f,w,x;a.parameters={...a.parameters,docs:{...(f=a.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xs'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(x=(w=a.parameters)==null?void 0:w.docs)==null?void 0:x.source}}};var h,v,j;s.parameters={...s.parameters,docs:{...(h=s.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'sm'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(j=(v=s.parameters)==null?void 0:v.docs)==null?void 0:j.source}}};var S,b,N;n.parameters={...n.parameters,docs:{...(S=n.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'md'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(N=(b=n.parameters)==null?void 0:b.docs)==null?void 0:N.source}}};var y,V,k;i.parameters={...i.parameters,docs:{...(y=i.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'lg'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(k=(V=i.parameters)==null?void 0:V.docs)==null?void 0:k.source}}};var C,I,L;m.parameters={...m.parameters,docs:{...(C=m.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xl'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(L=(I=m.parameters)==null?void 0:I.docs)==null?void 0:L.source}}};const pr=["Default","XSmall","Small","Medium","Large","XLarge"];export{o as Default,i as Large,n as Medium,s as Small,m as XLarge,a as XSmall,pr as __namedExportsOrder,mr as default};
