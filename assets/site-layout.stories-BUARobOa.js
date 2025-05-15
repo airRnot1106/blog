605f45324c5890d274f5ce360499c8940b60b9c6
airRnot1106 <airRnot1106@users.noreply.github.com>
2025-05-15 02:27:45 +0000

    deploy: 7ba5affafedfd82d192c92dafa504502e23fa85b

---
+++ assets/site-layout.stories-BUARobOa.js
@@ -0,0 +1,57 @@
+import{j as r}from"./jsx-runtime-DoEZbXM1.js";import{c as T}from"./css-iA79nXp1.js";import{S as D}from"./site-footer-Cv2LJNPq.js";import{S as E}from"./site-header-Ps01JB78.js";import"./jsx-runtime-Bw5QeaCk.js";import"./cx-BFURdk0p.js";import"./link-CiQcZn3-.js";import"./_commonjsHelpers-CqkleIqs.js";import"./router-context.shared-runtime-C3rDQPqq.js";import"./index-Za3YRiuF.js";import"./add-base-path-Yz4dcL5G.js";import"./use-merged-ref-vu_KmdPm.js";import"./site-utility-list-BKPSMnnp.js";import"./link-button-DcK4i4oQ.js";import"./ghost-link-button-B4OS-esE.js";import"./link-button-C7RiLLtY.js";import"./button-abs7jE4F.js";import"./ghost-button-DVSEpKed.js";import"./button-CFgMdbM0.js";import"./cva-CuDB4TaM.js";import"./normal-button-Be3dRcwo.js";import"./normal-link-button-CMCePiL0.js";import"./site-theme-toggle-button-Ewb3LVKk.js";import"./index-DDr4uD8U.js";import"./panic-helper-BEWS6D6P.js";import"./contexts-C6b2_pfF.js";import"./createLucideIcon-dVToDc26.js";import"./consts-CV3m3YIn.js";import"./newspaper-AlF7AAQn.js";import"./site-drawer-B1xsGiHs.js";import"./index-CGoK48M4.js";import"./index-BZkcKs8Z.js";import"./index-DyNXmn1L.js";import"./index-DqQFE8CJ.js";import"./site-navigation-link-list-Bu5JpNoT.js";import"./anchor-site-navigation-link-list-Cnl8d6G5.js";import"./site-navigation-link-DMIFXQJw.js";import"./button-site-navigation-link-list-Br9_3o77.js";const i=({children:_,className:N,...R})=>r.jsxs("div",{className:T({display:"grid",gridTemplateRows:"auto 1fr auto",minHeight:"100dvh"}),...R,children:[r.jsx(E,{}),r.jsx("div",{className:N,children:_}),r.jsx(D,{})]});i.__docgenInfo={description:"",methods:[],displayName:"SiteLayout",props:{children:{required:!0,tsType:{name:"ReactNode"},description:""}}};const Sr={component:i,tags:["autodocs"],parameters:{layout:"fullscreen"}},X=()=>r.jsxs("main",{children:[r.jsx("h1",{children:"Site Layout"}),r.jsx("p",{children:"This is the main content area."})]}),t={render:()=>r.jsx(i,{children:r.jsx(X,{})})},e=r.jsx(i,{children:r.jsx(X,{})}),o={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"xs"}},render:()=>e},a={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"sm"}},render:()=>e},s={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"md"}},render:()=>e},n={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"lg"}},render:()=>e},p={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"xl"}},render:()=>e};var m,d,c;t.parameters={...t.parameters,docs:{...(m=t.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => {
+    return <SiteLayout>
+        <Main />
+      </SiteLayout>;
+  }
+}`,...(c=(d=t.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var u,l,g;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xs'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(g=(l=o.parameters)==null?void 0:l.docs)==null?void 0:g.source}}};var w,f,x;a.parameters={...a.parameters,docs:{...(w=a.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'sm'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(x=(f=a.parameters)==null?void 0:f.docs)==null?void 0:x.source}}};var S,h,j;s.parameters={...s.parameters,docs:{...(S=s.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'md'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(j=(h=s.parameters)==null?void 0:h.docs)==null?void 0:j.source}}};var v,V,y;n.parameters={...n.parameters,docs:{...(v=n.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'lg'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(y=(V=n.parameters)==null?void 0:V.docs)==null?void 0:y.source}}};var L,C,M;p.parameters={...p.parameters,docs:{...(L=p.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xl'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(M=(C=p.parameters)==null?void 0:C.docs)==null?void 0:M.source}}};const hr=["Default","XSmall","Small","Medium","Large","XLarge"];export{t as Default,n as Large,s as Medium,a as Small,p as XLarge,o as XSmall,hr as __namedExportsOrder,Sr as default};
