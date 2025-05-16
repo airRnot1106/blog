72abd98400cfc3b12a739d254cee10a6607651dc
airRnot1106 <yngw.rintaro@gmail.com>
2025-05-16 09:42:54 +0900

    chore(api): add API_BASE_URL from env

--- src/libs/api/api.ts
+++ src/libs/api/api.ts
@@ -2,6 +2,9 @@ import 'server-only';
 
 import { shouldNeverHappen } from '../../utils/panic-helper';
 
+export const API_BASE_URL =
+  process.env.API_BASE_URL ?? shouldNeverHappen('API_BASE_URL is not defined');
+
 export const QIITA_API_KEY =
   process.env.QIITA_API_KEY ??
   shouldNeverHappen('QIITA_API_KEY is not defined');
