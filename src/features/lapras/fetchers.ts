a183bddcef979d2b29fe4a6a9133f7bf45669320
airRnot1106 <yngw.rintaro@gmail.com>
2025-05-16 12:33:13 +0900

    refactor: add server-only caching for data fetching

--- src/features/lapras/fetchers.ts
+++ src/features/lapras/fetchers.ts
@@ -1,13 +1,16 @@
+import 'server-only';
+
+import { cache } from 'react';
 import { getOgp } from '../../utils/ogp';
 import { mapResult } from '../../utils/result';
 
 const BASE_URL = 'https://lapras.com/public/airrnot';
 
-export const getLaprasImage = async () => {
+export const getLaprasImage = cache(async () => {
   const result = mapResult(
     await getOgp(BASE_URL),
     ({ result }) => result.ogImage.url,
   );
 
   return result;
-};
+});
