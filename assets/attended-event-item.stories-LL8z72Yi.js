efef88e1dabb97a9005db88955ebf1f174c8deb4
airRnot1106 <airRnot1106@users.noreply.github.com>
2025-07-25 15:40:22 +0000

    deploy: 7403467db6b8b2f314cc240926ef62f81cd7ac65

---
+++ assets/attended-event-item.stories-LL8z72Yi.js
@@ -0,0 +1,11 @@
+import{j as a}from"./jsx-runtime-DoEZbXM1.js";import{c as u}from"./css-iA79nXp1.js";import{A as s,a as c}from"./attended-event-item-C8jGhYAX.js";import"./jsx-runtime-Bw5QeaCk.js";import"./cx-BFURdk0p.js";import"./link-D8HMmSCG.js";import"./underline-link-BKNhLiPR.js";import"./link-DOkctWu4.js";import"./link-CiQcZn3-.js";import"./_commonjsHelpers-CqkleIqs.js";import"./router-context.shared-runtime-C3rDQPqq.js";import"./index-Za3YRiuF.js";import"./add-base-path-Yz4dcL5G.js";import"./use-merged-ref-vu_KmdPm.js";import"./datetime-At4GKbE0.js";import"./panic-helper-BEWS6D6P.js";const O={component:s,tags:["autodocs"],parameters:{layout:"fullscreen"},decorators:[e=>a.jsx("div",{className:u({padding:"1rem"}),children:a.jsx(e,{})})]},t={render:()=>{const e=c[0];return a.jsx(s,{dates:e.dates.map(n=>new Date(n)),title:e.title,url:e.url})}},r={render:()=>{const e=c[2];return a.jsx(s,{dates:e.dates.map(n=>new Date(n)),title:e.title,url:e.url})}};var o,d,m;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  render: () => {
+    const event = attendedEvents[0];
+    return <AttendedEventItem dates={event.dates.map(date => new Date(date))} title={event.title} url={event.url} />;
+  }
+}`,...(m=(d=t.parameters)==null?void 0:d.docs)==null?void 0:m.source}}};var p,i,l;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => {
+    const event = attendedEvents[2];
+    return <AttendedEventItem dates={event.dates.map(date => new Date(date))} title={event.title} url={event.url} />;
+  }
+}`,...(l=(i=r.parameters)==null?void 0:i.docs)==null?void 0:l.source}}};const R=["Default","MultipleDates"];export{t as Default,r as MultipleDates,R as __namedExportsOrder,O as default};
